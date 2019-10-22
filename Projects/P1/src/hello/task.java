76f3d91efee13c8f655cba086d3ff708880a0cc4
AreebSiddiqui <areeb40@hotmail.com>
2019-10-23 00:21:39 +0500

    added project 2

---
+++ Projects/P1/src/hello/task.java
@@ -0,0 +1,78 @@
+package hello;
+import java.util.Scanner;
+public class task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*for(int i =0 ;i <= 5; ++i) {
+			for(int j=1;j<=i;++j) {
+				System.out.print(j);
+				
+			}
+			System.out.print("\n");
+		}*/
+		
+		for(int i =5 ;i >= 0; --i) {
+			for(int j=1;j<=i;++j) {
+				System.out.print(j);
+				
+			}
+			System.out.print("\n");
+		}
+		
+		
+		
+		
+		/*float maths =90;
+		float science =100;
+		float chemistry =87;
+		float total = maths +chemistry+science;
+		float percentage = (total/300) *100;
+		System.out.println(percentage); */
+		
+		
+		/*int number = (int)(Math.random() * 3);
+		System.out.print(number);
+		Scanner inp = new Scanner(System.in);
+		int input = inp.nextInt();
+		if (input == 0 && number == 1) {
+			System.out.println("Sissor(0), rock(1), paper(2)");
+			System.out.println("The computer is scissor, you are rock you won");
+		}
+		else if (input == 1 && number == 0) {
+			System.out.println("Sissor(0), rock(1), paper(2)");
+			System.out.println("The computer is rock, you are scissor you lose");
+		}*/
+		
+		
+		
+		/*Scanner inp = new Scanner(System.in);
+		int input = inp.nextInt();
+		
+		if (input == 0) {
+		Scanner i = new Scanner(System.in);
+		int c = i.nextInt();
+		
+		int farienhiet  = c * (9/5) + 32;
+			System.out.println(farienhiet);
+
+		}
+		
+		else if (input == 1) {
+			Scanner i = new Scanner(System.in);
+			int f = i.nextInt();
+			
+			int celcius  = f * (5/9) - 32;
+				System.out.println(celcius);
+				
+
+			
+		}*/
+		
+		
+		
+		
+		
+}
+}
